0bd4927fd51c26402c633224813f9dcb3bf6329e
Grabbit1222 <133847810+Grabbit1222@users.noreply.github.com>
2025-03-24 14:58:13 +0900

    [Add] Project and Basic Settings

---
+++ Source/CCFF/CCFF.Build.cs
@@ -0,0 +1,16 @@
+using UnrealBuildTool;
+
+public class CCFF : ModuleRules
+{
+	public CCFF(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+
+        PublicIncludePaths.AddRange(new string[]
+        {
+            "CCFF",
+		});
+    }
+}
