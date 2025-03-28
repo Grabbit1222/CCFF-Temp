24e953417be443c6e4fb50c134db79ea811d64ce
2h1824 <hongil0824@naver.com>
2025-03-28 16:01:13 +0900

    Revert "[Update] Character Base Class and Basic Move function"
    This reverts commit 8a3b5181ee5f0b48a4c3175d2628dd0c71f6b3d4.

--- Source/CCFF/CCFFCharacter.h
+++ Source/CCFF/CCFFCharacter.h
@@ -15,7 +15,6 @@ struct FInputActionValue;
 
 DECLARE_LOG_CATEGORY_EXTERN(LogTemplateCharacter, Log, All);
 
-
 UCLASS(config=Game)
 class ACCFFCharacter : public ACharacter
 {
@@ -56,7 +55,7 @@ protected:
 
 	/** Called for looking input */
 	void Look(const FInputActionValue& Value);
-
+			
 
 protected:
 
